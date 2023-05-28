57689480a0976f8b203e88eb64f91908d2947f56
yashcrest <yashcrest@gmail.com>
2023-05-28 22:39:17 +0930

    functionaing weather app is complete. Pending: to add weather background, align line items, change the first page view.

--- weather.js
+++ weather.js
@@ -1,5 +1,26 @@
 const container = document.querySelector(".container");
 const apiKey = "34d6d8dc8a584ef6b4d92016232105"
+const userInput = document.querySelector("#userInput");
+const searchBtn = document.querySelector("#searchBtn");
+const celcius = document.querySelector(".celcius");
 
 
 //get user input
+searchBtn.addEventListener('click', getWeather);
+userInput.addEventListener('keyup' , ((e) =>{
+    if(e.key ==='Enter'){
+        getWeather();
+    }
+}))
+
+function getWeather() {
+    fetch(`https://api.weatherapi.com/v1/current.json?q=${userInput.value}&key=${apiKey}`).then(res => res.json()).then(data => {
+        console.log(data);
+        let location = data.location.name
+        let celcisData = data.current.temp_c;
+        celcius.textContent = `${celcisData} °C`;
+        let img = document.createElement('img')
+        img.src = data.current.condition.icon;
+        container.appendChild(img);
+    }).catch(err => console.log(err));
+}
